a8011fcaa83dc7e6762250286c64bd4ff2cca026
LieutenantPeacock <lieutenant.peacock@gmail.com>
2021-07-23 10:09:49 -0400

    ArrayList.js: fixed indenting.

--- src/ArrayList.js
+++ src/ArrayList.js
@@ -9,84 +9,84 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 class ArrayList {
 	#arr
-	
-	constructor(arg){
+
+	constructor(arg) {
 		this.#arr = arg && [...arg] || [];
 	}
-	
-	add(...elems){
+
+	add(...elems) {
 		this.#arr.push(...elems);
 	}
-	
-	addAll(obj){
+
+	addAll(obj) {
 		this.add(...obj);
 	}
-	
-	removeAtIndex(idx){
-		if(idx >= 0 && idx < this.#arr.length){
+
+	removeAtIndex(idx) {
+		if (idx >= 0 && idx < this.#arr.length) {
 			this.#arr.splice(idx, 1);
 		}
 	}
-	
-	get(idx){
+
+	get(idx) {
 		return this.#arr[idx];
 	}
-	
-	addAtIndex(idx, ...elems){
+
+	addAtIndex(idx, ...elems) {
 		this.#arr.splice(idx, 0, ...elems);
 	}
-	
-	set(idx, elem){
+
+	set(idx, elem) {
 		this.#arr[idx] = elem;
 	}
-	
-	removeIf(predicate){
-		this.#arr = this.#arr.filter((elem,idx)=>!predicate(elem,idx));
+
+	removeIf(predicate) {
+		this.#arr = this.#arr.filter((elem, idx) => !predicate(elem, idx));
 	}
-	
-	remove(obj){
+
+	remove(obj) {
 		const idx = indexOf(obj);
-		if(idx !== -1) this.#arr.splice(idx, 1);
+		if (idx !== -1) this.#arr.splice(idx, 1);
 	}
-	
-	indexOf(obj){
-		for(let i = 0; i < this.#arr.length; i++){
-			if(this.#arr[i] === obj || typeof this.#arr[i]?.equals === 'function' && this.#arr[i].equals(obj))
+
+	indexOf(obj) {
+		for (let i = 0; i < this.#arr.length; i++) {
+			if (this.#arr[i] === obj || typeof this.#arr[i]?.equals === 'function' && this.#arr[i].equals(obj))
 				return i;
 		}
 		return -1;
 	}
-	
-	size(){
+
+	size() {
 		return this.#arr.length;
 	}
-	
-	isEmpty(){
+
+	isEmpty() {
 		return this.#arr.length === 0;
 	}
-	
-	hashCode(){
-        let hashCode = 1;
-        for (const element of this)
-            hashCode = 31 * hashCode + 
-            (typeof element.hashCode === 'function' ? element.hashCode() : 
-            	!isNaN(+element) ? +element : 0);
-        return hashCode;
+
+	hashCode() {
+		let hashCode = 1;
+		for (const element of this)
+			hashCode = 31 * hashCode +
+				(typeof element.hashCode === 'function' ? element.hashCode() :
+					!isNaN(+element) ? +element : 0);
+		return hashCode;
 	}
-	
-	equals(obj){
-		if(obj.constructor !== ArrayList || this.size() != obj.size()) return false;
-		for(let i = 0, size = this.size(); i < size; i++){
+
+	equals(obj) {
+		if (obj.constructor !== ArrayList || this.size() != obj.size()) return false;
+		for (let i = 0, size = this.size(); i < size; i++) {
 			const a = this.get(i), b = obj.get(i);
-			if(a !== b && 
-				(typeof a?.equals !== 'function' || !a.equals(b))){
+			if (a !== b &&
+				(typeof a?.equals !== 'function' || !a.equals(b))) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
-	[Symbol.iterator](){
+
+	[Symbol.iterator]() {
 		return this.#arr.values();
 	}
 }
